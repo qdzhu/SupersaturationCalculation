5d54b2e464da204581c09072cc81bf292105c101
Qindan Zhu <qindan_zhu@berkeley.edu>
2019-11-16 11:20:53 -0800

    implements more functions for halo

--- misc_halo_analysis.m
+++ misc_halo_analysis.m
@@ -13,6 +13,14 @@ classdef misc_halo_analysis
             value = fullfile(misc_halo_analysis.box_dir, 'HALO_cleanup_csv');
         end
         
+        function value = katie_workspace_dir()
+            value = fullfile(misc_halo_analysis.box_dir,'Workspace_KT');
+        end
+        
+        function value = my_workspace_dir()
+            value = fullfile(misc_halo_analysis.box_dir,'/AtmosDynamic/Workspace');
+        end
+        
         function Dp = cdp_diameter()
             Dp = struct('low',[2.5, 2.9, 5, 7.5, 10.2, 11.8, 15.6, 18.7, 20.7, 24.6, 27.4, 29.2, 34.4, 39, 42.5],...
                         'up',[2.9, 5, 7.5, 10.2, 11.8, 15.6, 18.7, 20.7, 24.6, 27.4, 29.2, 34.4, 39, 42.5, 46],...
@@ -129,6 +137,10 @@ classdef misc_halo_analysis
             casdata.nconc = zeros(size(casdata.utcsec));
             casdata.lwc = file(:,end-3);
             casdata.lwc(casdata.lwc<0) = nan;
+            casdata.PAS = file(:,end-2);
+            casdata.TAS = file(:, end-1);
+            casdata.ptratio = casdata.PAS./casdata.TAS;
+            casdata.Xi = file(:,end);
             lowdp = misc_halo_analysis.cas_diameter.low;
             updp = misc_halo_analysis.cas_diameter.up;
             meandp = (lowdp+updp)/2/2;%convert from diameter to radius
@@ -145,6 +157,7 @@ classdef misc_halo_analysis
             end
             casdata.meandp = meandp_time_nconc./nconc;
             casdata.nconc = nconc;
+            casdata.nconc_corr = nconc./casdata.ptratio.*casdata.Xi;
         end
         
         function adlrdata = read_adlr_data(adlrname)
@@ -168,6 +181,39 @@ classdef misc_halo_analysis
             end
         end
         
+        function collect = collect_allday(match,filter_opt)
+            collect = make_empty_struct_from_cell(fieldnames(match));
+            fields = fieldnames(match);
+            for i_field=1:numel(fields)
+                for i_day = 1:numel(match)
+                    collect.(fields{i_field}) = cat(2, collect.(fields{i_field}), match(i_day).(fields{i_field}) );
+                end
+            end
+            
+            if strcmpi(filter_opt,'sim')
+                filter_arr =  collect.cdp_meandp >=1 ...
+                    & collect.cas_meandp >= 1 & collect.cdp_nconc >=10 ...
+                    & collect.cas_nconc >=10 & collect.alt>0;
+%                 filter_arr = collect.alt>0;
+                for i_field=1:numel(fields)
+                    thisdata = collect.(fields{i_field});
+                    thisdata(~filter_arr) = nan;
+                    collect.(fields{i_field}) = thisdata;
+                end
+            end
+        end
+        
+        function value_bins = bin_vertical(value, alt_all, quantile, alt_bins)
+            half_bin = (alt_bins(2)-alt_bins(1))/2;
+            value_bins = zeros(1,numel(alt_bins));
+            
+            for i=1:numel(alt_bins)
+                value_threshold = prctile(value(alt_all>alt_bins(i)-half_bin & alt_all<=alt_bins(i)+half_bin),quantile);
+                indx = alt_all>alt_bins(i)-half_bin & alt_all<=alt_bins(i)+half_bin & value>=value_threshold;
+
+                value_bins(i) = nanmean(value(indx));
+            end
+        end
         %%%%%%%%%%%%%%%%%%%
         % Make methods    %
         %%%%%%%%%%%%%%%%%%%
@@ -191,6 +237,9 @@ classdef misc_halo_analysis
                 %cas_indx = ismember(casdata.utcsec, match(i_date).utcsec);
                 match(i_date).cas_meandp = misc_halo_analysis.struct_filter(casdata, match(i_date).utcsec, 'meandp');
                 match(i_date).cas_nconc = misc_halo_analysis.struct_filter(casdata, match(i_date).utcsec,'nconc');
+                match(i_date).cas_nconc_corr = misc_halo_analysis.struct_filter(casdata, match(i_date).utcsec,'nconc_corr');
+                match(i_date).cas_ptratio = misc_halo_analysis.struct_filter(casdata, match(i_date).utcsec,'ptratio');
+                match(i_date).cas_Xi = misc_halo_analysis.struct_filter(casdata, match(i_date).utcsec,'Xi');
                 match(i_date).lwc = misc_halo_analysis.struct_filter(casdata, match(i_date).utcsec, 'lwc');
                 %adlr_indx = ismember(adlrdata.utcsec, match(i_date).utcsec);
                 match(i_date).temp = misc_halo_analysis.struct_filter(adlrdata, match(i_date).utcsec,'temp');
@@ -201,8 +250,11 @@ classdef misc_halo_analysis
         end
         
         function make_supersaturation()
-            data = load('halo_match.mat');
+            data = load(fullfile(misc_halo_analysis.katie_workspace_dir,'halo_match_v2.mat'));
             match = data.match;
+            
+            data = load(fullfile(misc_halo_analysis.my_workspace_dir,'halo_match.mat'));
+            corr = data.match;
             [a0,a1,a2,a3,a4,a5,a6,Rg,Ra,Cpa,Mma,Rv,Cpv,Mmv,pl,ps,Mms,alpha,w,Po,To,g,k_mu,k_ml]=recalculate_CAIPEEX_result.Constant;
             for i_date=1:numel(match)
                  T = match(i_date).temp;
@@ -220,16 +272,179 @@ classdef misc_halo_analysis
                  match(i_date).cdp_SS = A.*vel./(4*pi*D.*cdp_meandp.*cdp_nconc)*100;
                  cas_meandp = match(i_date).cas_meandp;
                  cas_nconc = match(i_date).cas_nconc;
+                 cas_nconc_corr = corr(i_date).cas_nconc_corr;
                  cas_meandp(cas_meandp == 0) =nan;
                  cas_nconc(cas_nconc ==0) = nan;
+                 cas_nconc_corr(cas_nconc_corr ==0) = nan;
                  match(i_date).cas_SS = A.*vel./(4*pi*D.*cas_meandp.*cas_nconc)*100;
+                 match(i_date).cas_SS_corr = A.*vel./(4*pi*D.*cas_meandp.*cas_nconc_corr)*100;
+                 match(i_date).cas_nconc_corr = corr(i_date).cas_nconc_corr;
             end
-            save('halo_match_ss.mat','match');
+            save(fullfile(misc_halo_analysis.my_workspace_dir,'halo_match_v3.mat'),'match');
         end
         
+        function make_measurement_time_per_day()
+            data = load('halo_match_v2.mat');
+            match = data.match;
+            time = make_empty_struct_from_cell({'utcsec_min','date','utcsec_max'});
+            
+            for i=1:numel(match)
+                time.date = [datenum(time.date); datenum(match(i).date)];
+                time.utcsec_min = [time.utcsec_min; min(match(i).utcsec)];
+                time.utcsec_max = [time.utcsec_max; max(match(i).utcsec)];
+            end
+            
+            save('measurement_time_per_day.mat', 'time');
+        end
         %%%%%%%%%%%%%%%%%%%
         % Plot methods    %
         %%%%%%%%%%%%%%%%%%%
+        function fig = plot_supersaturation_helper()
+            
+        end
+        
+        function fig = plot_supersaturation_exp()
+            data = load(fullfile(misc_halo_analysis.my_workspace_dir,'halo_match_v3.mat'));
+            match = data.match;
+            filter_opt = 'sim';
+            collect = misc_halo_analysis.collect_allday(match, filter_opt);
+            
+            figure(1);
+            subplot(2,2,1);
+            scatter(collect.cdp_meandp*2, collect.cas_meandp*2,3,'MarkerFaceColor',TolColorScheme.fav.blue,...
+                'LineWidth',0.2,'MarkerEdgeColor',TolColorScheme.fav.blue);
+            plot_fit_line(collect.cdp_meandp*2, collect.cas_meandp*2)
+            xlabel('CDP');
+            ylabel('CAS');
+            title('Mean Diameter (microns)');
+            
+            subplot(2,2,2);
+            scatter(collect.cdp_nconc, collect.cas_nconc,3,'MarkerFaceColor',TolColorScheme.fav.yellow,...
+                'LineWidth',0.2,'MarkerEdgeColor',TolColorScheme.fav.green);
+            plot_fit_line(collect.cdp_nconc, collect.cas_nconc);
+%             line([0,50],[0,50],'linestyle','--','color','red','linewidth',2);
+            xlabel('CDP');
+            ylabel('CAS');
+            title('Number Concentration (#cm^{-3})');
+            
+            subplot(2,2,3);
+            scatter(collect.cas_nconc, collect.cas_nconc_corr,3,'MarkerFaceColor',TolColorScheme.fav.yellow,...
+                'LineWidth',0.2,'MarkerEdgeColor',TolColorScheme.fav.green);
+            plot_fit_line(collect.cas_nconc, collect.cas_nconc_corr);
+%             line([0,50],[0,50],'linestyle','--','color','red','linewidth',2);
+            xlabel('CAS');
+            ylabel('CAS Corrected');
+            title('Number Concentration (#cm^{-3})');
+            
+            subplot(2,2,4);
+            scatter(collect.cdp_nconc, collect.cas_nconc_corr,3,'MarkerFaceColor',TolColorScheme.fav.yellow,...
+                'LineWidth',0.2,'MarkerEdgeColor',TolColorScheme.fav.green);
+            plot_fit_line(collect.cdp_nconc, collect.cas_nconc_corr);
+%             line([0,50],[0,50],'linestyle','--','color','red','linewidth',2);
+            xlabel('CDP');
+            ylabel('CAS Corrected');
+            title('Number Concentration (#cm^{-3})');
+            
+            subplot(2,2,3);
+            line(collect.cdp_nconc.*collect.cdp_meandp, collect.cas_nconc.*collect.cas_meandp,'linestyle','none','marker','o','markersize',2,'color','blue');
+%             line([0,50],[0,50],'linestyle','--','color','red','linewidth',2);
+            xlabel('CDP');
+            ylabel('CAS');
+            title('Mean radius \times number concentration');
+            
+            
+            figure(1);
+            scatter(collect.cdp_SS, collect.w,[],collect.alt/1000, 'filled');
+            line([-8,10],[0,0],'color','r','linestyle','--','linewidth',2);
+            line([0,0],[-10,20],'color','r','linestyle','--','linewidth',2);
+            h = colorbar;
+%             caxis([0,0.1])
+            xlabel('SS,%');
+            ylabel(h,'Altitude (km)');
+            ylabel('W (m/s)');
+            
+            figure(2);
+            hold on;
+            mean_ss = (collect.cdp_SS+collect.cas_SS)/2;
+            errorbar_ss = abs(collect.cdp_SS-collect.cas_SS)/2;
+            scatter_errorbars(mean_ss,collect.w,errorbar_ss,'direction','x','color','k');
+            scatter(mean_ss, collect.w,[],collect.alt/1000, 'filled');
+            line([-8,10],[0,0],'color','r','linestyle','--','linewidth',2);
+            line([0,0],[-10,20],'color','r','linestyle','--','linewidth',2);
+            h = colorbar;
+%             caxis([0,0.1])
+            xlabel('SS,%');
+            ylabel(h,'Altitude (km)');
+            ylabel('W (m/s)');
+            hold off;
+            
+            figure(3);
+            histogram(collect.cdp_lwc_calc*1e6);
+            xlabel('LWC (g/m^{3})');
+            ylabel('frequencey');
+            
+            figure(4);
+            edge = collect.cdp_lwc_calc <= prctile(collect.cdp_lwc_calc,5);
+            nonedge = collect.cdp_lwc_calc > prctile(collect.cdp_lwc_calc,5);
+            hold on;
+            histogram(collect.cdp_SS(nonedge),30,'Normalization','probability');
+            histogram(collect.cdp_SS(edge),30,'Normalization','probability');
+            
+            legend('Non-Edge','Edge');
+            xlabel('SS from CDP (%)');
+            ylabel('frequency');
+        end
+        
+        function fig = plot_supersaturation_edge()
+            data = load(fullfile(misc_halo_analysis.my_workspace_dir,'halo_match_v3.mat'));
+            match = data.match;
+            filter_opt = 'sim';
+            collect = misc_halo_analysis.collect_allday(match, filter_opt);
+            
+            figure;
+            [cdp_edge_ss, cdp_center_ss] = recalculate_CAIPEEX_result.identify_edge_center(collect.cdp_SS',...
+                collect.alt', collect.cdp_lwc_calc');
+            kernest_center = fitdist(cdp_center_ss, 'kernel');
+            kernest_edge = fitdist(cdp_edge_ss, 'kernel');
+            ss = -6:0.01:6;
+            
+            subplot(1,2,1);
+            hold on;
+            line(ss, kernest_center.pdf(ss),'linestyle','-','color',TolColorScheme.fav.blue,'linewidth',2);
+            line(ss, kernest_edge.pdf(ss),'linestyle','-','color',TolColorScheme.fav.red,'linewidth',2);
+%             histogram(cdp_center_ss,30,'Normalization','probability');
+%             histogram(cdp_edge_ss,30,'Normalization','probability');
+            legend('Non-Edge','Edge');
+            xlabel('Supersaturation from CDP (%)');
+            ylabel('frequency');
+            hold off;
+            
+            subplot(1,2,2);
+            [cas_edge_ss, cas_center_ss] = recalculate_CAIPEEX_result.identify_edge_center(collect.cas_SS',...
+                collect.alt', collect.cas_lwc_calc');
+            kernest_center = fitdist(cas_center_ss, 'kernel');
+            kernest_edge = fitdist(cas_edge_ss, 'kernel');
+            ss = -6:0.01:6;
+            hold on;
+            line(ss, kernest_center.pdf(ss),'linestyle','-','color',TolColorScheme.fav.blue,'linewidth',2);
+            line(ss, kernest_edge.pdf(ss),'linestyle','-','color',TolColorScheme.fav.red,'linewidth',2);
+            legend('Non-Edge','Edge');
+%             histogram(cas_center_ss,30,'Normalization','probability');
+%             histogram(cas_edge_ss,30,'Normalization','probability');
+            xlabel('Supersaturation from CAS (%)');
+            ylabel('frequency');
+            hold off;
+            
+            figure;
+            hold on;
+            scatter(collect.cdp_SS, collect.cas_SS, 20,...
+                'LineWidth',0.2,'MarkerEdgeColor',TolColorScheme.fav.blue);
+            line([-10,10],[0,0],'color',TolColorScheme.fav.red, 'linewidth',2,'linestyle','--');
+            line([0,0],[-10,20],'color',TolColorScheme.fav.red, 'linewidth',2,'linestyle','--');
+            xlabel('CDP Supersaturation (%)');
+            ylabel('CAS Supersaturation (%)');
+        end
+        
         function plot_updraft_wind_supersaturation_oneday(n)
             data = load('halo_match_ss.mat');
             match = data.match(n);
@@ -357,6 +572,55 @@ classdef misc_halo_analysis
                   'showgrid', false, ...
                   'zeroline', false));
              response = plotly(data, struct('layout', layout, 'fileopt', 'overwrite'));
+        end
+        
+        function plot_diff_with_aerosol(aer_field)
+            data = load('halo_match_ss.mat');
+            match = data.match;
+            
+            data = load('aerosol_conc.mat');
+            aer = data.aer;
+            alt_bins = linspace(1,18,40);
+            
+            clist = colormap(jet(8));
+            plist = linspace(1000, 8000, 8);
+            pres2clist = interp1(plist,clist,aer.(aer_field)(~isnan(aer.(aer_field))),'linear');
+            k = 1;
+            
+            for i=1:numel(match)
+                if isnan(aer.(aer_field)(i))
+                    continue;
+                end
+                ss = match(i).cdp_SS;
+                w = match(i).w;
+                ss(match(i).cdp_nconc < 10) = nan;
+                w(match(i).cdp_nconc < 10) = nan;
+                ss_bin = misc_halo_analysis.bin_vertical(ss, match(i).alt/1e3, 90, alt_bins);
+                vel_bin = misc_halo_analysis.bin_vertical(w, match(i).alt/1e3, 90, alt_bins);
+               
+                subplot(1,2,1);
+                hold on;
+                plot(ss_bin, alt_bins,'color',pres2clist(k,:));
+                
+                
+                subplot(1,2,2);
+                hold on;
+                plot(vel_bin, alt_bins,'color',pres2clist(k,:));
+                
+                k = k+1;
+                
+            end
+            subplot(1,2,1);
+            xlabel('Supersaturation (%)');
+            ylabel('Altitude (m)');
+            subplot(1,2,2);
+            h = colorbar;
+            h.Ticks = (1:8)/8;
+            h.TickLabels = num2cell(plist(1:8));
+            ylabel(h,'Aerosol number concentration (#cm^{-3})');
+            xlabel('Vertical Wind velocity (m/s)');
+            ylabel('Altitude (m)');
+            hold off;
         end
     end
 end
